cb0f010787ed8db5aafa8b35052c60ecbe19d4eb
mueid22110 <mueidhasan9@gmail.com>
2024-10-06 23:02:42 +0600

    Cpp day 2

---
+++ second.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+// This is the header that stores all the necessary headers.
+int main(){
+	
+	int number = 1090;
+
+	cout << number << endl;
+	
+	int length = 20; // length of a rectangle
+	int width = 30; // width of a rectangle
+	int area = length * width;
+	
+	cout << "The area of the rectangle is " << area << endl;
+	
+	// Now the area of a circle
+	double pi = 3.1416;
+	double radius = 8.9;
+	double Area = pi *radius*radius;
+	
+	cout << "The area of the circle is " << Area << endl;
+	
+	
+}
